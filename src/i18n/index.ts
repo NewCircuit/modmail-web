b15893b879937ef9c45c9b26414103b072adc072
Devin <demitchell14@gmail.com>
2021-01-07 23:45:22 -0600

    changed i18n to use raw json files instead of injecting into javascript

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,5 @@
-import i18next, { Resource } from 'i18next';
+import i18next from 'i18next';
+import i18nBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import common from './en/common.json';
 import errors from './en/errors.json';
@@ -15,21 +16,26 @@ export const resources: Resource = {
 };
 
 export function init() {
-    return i18next.use(initReactI18next).init({
-        lng: 'en',
-        ns: ['common', 'errors', 'layout', 'unauthorized'],
-        debug: true,
-        resources,
-        react: {
-            // transKeepBasicHtmlNodesFor: ['b', 'br', 'u', 'span'],
-        },
-        interpolation: {
-            format(value, format) {
-                if (format === 'firstUpper') {
-                    return value.charAt(0).toUpperCase() + value.substr(1);
-                }
-                return value;
+    return i18next
+        .use(initReactI18next)
+        .use(i18nBackend)
+        .init({
+            fallbackNS: false,
+            fallbackLng: false,
+            lng: 'en',
+            backend: {
+                loadPath: 'i18n/{{lng}}/{{ns}}.json',
             },
-        },
-    });
+            react: {
+                // transKeepBasicHtmlNodesFor: ['b', 'br', 'u', 'span'], // TODO add these as needed
+            },
+            interpolation: {
+                format(value, format) {
+                    if (format === 'firstUpper') {
+                        return value.charAt(0).toUpperCase() + value.substr(1);
+                    }
+                    return value;
+                },
+            },
+        });
 }
