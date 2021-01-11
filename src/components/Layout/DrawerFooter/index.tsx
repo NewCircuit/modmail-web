0845148d0d7fc5ec057c18641cec9aab518e5356
Devin <demitchell14@gmail.com>
2021-01-11 07:57:21 -0600

    start of drawer footer

---
+++ src/components/Layout/DrawerFooter/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton, Toolbar, Tooltip } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: 'auto',
+        padding: '0 .5rem',
+        borderTop: `1px solid ${theme.palette.divider}`,
+    },
+    refreshIcon: {
+        backgroundColor: theme.palette.primary.dark,
+        width: 50,
+        height: 50,
+        fontSize: '1.25rem',
+        lineHeight: '1.25rem',
+        '&:hover': {
+            backgroundColor: theme.palette.primary.main,
+        },
+    },
+}));
+
+export default function DrawerFooter(): JSX.Element {
+    const classes = useStyles();
+    useEffect(() => {
+        console.log('DrawerFooter');
+    }, []);
+    return (
+        <Toolbar disableGutters className={classes.root}>
+            <Tooltip title={'Time to refresh'}>
+                <IconButton className={classes.refreshIcon}>999</IconButton>
+            </Tooltip>
+        </Toolbar>
+    );
+}
