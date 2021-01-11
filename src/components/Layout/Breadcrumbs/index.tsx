29ef982e7e97772d96c17bc4f152099f3246a9d4
Devin <demitchell14@gmail.com>
2021-01-11 07:57:07 -0600

    set up visual demonstration

--- src/components/Layout/Breadcrumbs/index.tsx
+++ src/components/Layout/Breadcrumbs/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Breadcrumbs as BreadcrumbsBase, Link, Typography } from '@material-ui/core';
 
 type Props = any;
 
+// TODO make this functional
 export default function Breadcrumbs(props: Props) {
-    return <div>Breadcrumbs please</div>;
+    return (
+        <BreadcrumbsBase>
+            <Link href={'#'}>Home</Link>
+            <Link href={'#'}>Minecraft</Link>
+            <Typography color={'textPrimary'}>Threads</Typography>
+        </BreadcrumbsBase>
+    );
 }
