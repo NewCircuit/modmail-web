4ec4a366333d144f3c6397f1582bd7cc0a6646ba
Devin Mitchell <demitchell14>
2021-01-28 00:02:09 -0600

    temporary dummy data

--- src/components/Layout/ThreadsDrawer/index.tsx
+++ src/components/Layout/ThreadsDrawer/index.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect } from 'react';
 import { Slide } from '@material-ui/core';
+import ThreadListItem from '../../ThreadListItem';
+import { NavigationState } from '../../../state';
 
 export default function ThreadDrawer() {
+    const { threads } = NavigationState.useContainer();
+    const thread = threads.items[0];
     useEffect(() => {
         console.log('ThreadsDrawer');
     }, []);
     return (
         <Slide in direction={'right'}>
-            <div>Threads</div>
+            <div>
+                <ThreadListItem thread={thread} replied />
+                <ThreadListItem thread={thread} />
+                <ThreadListItem thread={thread} replied />
+                <ThreadListItem thread={thread} />
+                <ThreadListItem thread={thread} replied />
+                <ThreadListItem thread={thread} />
+            </div>
         </Slide>
     );
 }
