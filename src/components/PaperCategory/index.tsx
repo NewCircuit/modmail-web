e5e2e5789d7dcb7c7306d5f3a3af92581cf73699
Devin Mitchell <demitchell14@gmail.com>
2021-02-25 01:00:31 -0600

    thread items are represented as links now to allow users to open new tabs and such

--- src/components/PaperCategory/index.tsx
+++ src/components/PaperCategory/index.tsx
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Category } from '@Floor-Gang/modmail-types';
 import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 type Props = {
     category: Category;
@@ -13,6 +14,9 @@ type Props = {
 };
 
 const useStyles = makeStyles((theme) => ({
+    rootLink: {
+        textDecoration: 'none',
+    },
     categoryRoot: {
         '&:hover,&:focus': {
             backgroundColor: lighten(theme.palette.background.paper, 0.05),
@@ -66,70 +70,76 @@ function PaperCategory(props: Props) {
     const { category, className, onClick, elevation } = props;
     const classes = useStyles();
 
-    const handleClick = (evt: React.SyntheticEvent<HTMLDivElement>) => {
+    const handleClick = (evt: React.SyntheticEvent<HTMLLinkElement>) => {
         if (onClick) {
             onClick(evt, category);
         }
     };
 
     return (
-        <Paper
-            onClick={handleClick}
-            elevation={elevation}
-            className={clsx(classes.categoryRoot, className)}
+        <Link
+            className={classes.rootLink}
+            onClick={handleClick as any}
+            to={`/category/${category.id}/users/me/history`}
         >
-            <div className={classes.left}>
-                <Avatar className={classes.avatar}>{category.emojiID}</Avatar>
-            </div>
-            <div>
-                <Typography className={classes.name} variant={'h3'}>
-                    {category.name}
-                </Typography>
-                <Typography className={classes.description} variant={'subtitle1'}>
-                    {category.description}
-                </Typography>
-                <div className={classes.chipContainer}>
-                    <Tooltip
-                        title={
-                            t('tooltips.category.status', {
-                                category: category.name,
-                                status: category.isActive ? 'active' : 'inactive',
-                            }) as string
-                        }
-                    >
-                        <Chip
-                            size={'small'}
-                            className={clsx(classes.chip, {
-                                [classes.chipActive]: category.isActive,
-                            })}
-                            label={t(
-                                category.isActive ? 'chips.active' : 'chips.inactive'
-                            )}
-                        />
-                    </Tooltip>
-                    <Tooltip
-                        title={
-                            t(
-                                `tooltips.category.${
-                                    category.isPrivate ? 'private' : 'public'
-                                }`,
-                                { category: category.name }
-                            ) as string
-                        }
-                    >
-                        <Chip
-                            size={'small'}
-                            className={clsx(classes.chip, {
-                                [classes.chipPrivate]: category.isPrivate,
-                            })}
-                            label={t(
-                                category.isPrivate ? 'chips.private' : 'chips.public'
-                            )}
-                        />
-                    </Tooltip>
+            <Paper
+                onClick={handleClick as any}
+                elevation={elevation}
+                className={clsx(classes.categoryRoot, className)}
+            >
+                <div className={classes.left}>
+                    <Avatar className={classes.avatar}>{category.emojiID}</Avatar>
                 </div>
-            </div>
-        </Paper>
+                <div>
+                    <Typography className={classes.name} variant={'h3'}>
+                        {category.name}
+                    </Typography>
+                    <Typography className={classes.description} variant={'subtitle1'}>
+                        {category.description}
+                    </Typography>
+                    <div className={classes.chipContainer}>
+                        <Tooltip
+                            title={
+                                t('tooltips.category.status', {
+                                    category: category.name,
+                                    status: category.isActive ? 'active' : 'inactive',
+                                }) as string
+                            }
+                        >
+                            <Chip
+                                size={'small'}
+                                className={clsx(classes.chip, {
+                                    [classes.chipActive]: category.isActive,
+                                })}
+                                label={t(
+                                    category.isActive ? 'chips.active' : 'chips.inactive'
+                                )}
+                            />
+                        </Tooltip>
+                        <Tooltip
+                            title={
+                                t(
+                                    `tooltips.category.${
+                                        category.isPrivate ? 'private' : 'public'
+                                    }`,
+                                    { category: category.name }
+                                ) as string
+                            }
+                        >
+                            <Chip
+                                size={'small'}
+                                className={clsx(classes.chip, {
+                                    [classes.chipPrivate]: category.isPrivate,
+                                })}
+                                label={t(
+                                    category.isPrivate ? 'chips.private' : 'chips.public'
+                                )}
+                            />
+                        </Tooltip>
+                    </div>
+                </div>
+            </Paper>
+        </Link>
     );
 }
 
